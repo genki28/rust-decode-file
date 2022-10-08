b6c30d284952ff49929bc51e1a6a872802a479a7
genki28 <taiiku39@gmail.com>
2022-10-09 00:06:39 +0900

    コマンドライン引数の強化

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 extern crate url;
 
+use argh::FromArgs;
 use percent_encoding::{percent_decode, utf8_percent_encode, NON_ALPHANUMERIC};
 use std::str::Utf8Error;
 
+#[derive(FromArgs)]
+/// Reach new heights.
+struct GoUp {
+  /// whether or not to jump
+  #[argh(switch, short = 'j')]
+  jump: bool,
+
+  /// how high to go
+  #[argh(option)]
+  height: usize,
+
+  /// an optional nickname for the pilot
+  #[argh(option)]
+  pilot_nickname: Option<String>,
+
+  /// an optional direction which is "up" by default
+  #[argh(option, default = "String::from(\"only up\")")]
+  direction: String,
+}
+
 fn main() -> Result<(), Utf8Error> {
+  let args: GoUp = argh::from_env();
+  print!("jump to {:?}", args.jump);
+  print!("height to {:?}", args.height);
+  print!("pilot_nickname to {:?}", args.pilot_nickname);
+  print!("direction to {:?}", args.direction);
   let input = "test-sample-encoding";
 
   let iter = utf8_percent_encode(input, NON_ALPHANUMERIC);
